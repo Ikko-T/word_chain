bd09b88de5b05a1c770740f4e0364a49ddbe5494
Ikko-T <skyblueboy32@gmail.com>
2020-08-04 09:43:47 +0900

    ヒアドキュメント追加

--- instructions.rb
+++ instructions.rb
@@ -1,9 +1,20 @@
 class Instruction
   def greet
+    favorite_food = ["ゴーヤー", "サーターアンダギー", "ソーキそば", "フーチャンプルー", "ちんすこう"].sample
+    favorite_message = <<~TEXT
+    好きな食べ物は#{favorite_food}です。
+    #{"-" * 40}
+    TEXT
+    shisa_start = <<~TEXT
+    #{"-" * 40}
+    それでは、わたくしシーサーから始めます！
+    #{"=" * 40}
+    TEXT
+
     messages = [
       "めんそ〜れ〜",
       "しりとり名人の「シーサー・ゴヤ」と申します。",
-      "",
+      favorite_message,
       "それはさておき、しりとりゲームの説明をします。",
       "世界国名しりとりです。",
       "ルールは、",
@@ -11,23 +22,13 @@ class Instruction
       "語尾が「ン」で終った場合、",
       "同じ国名を二度繰り返した場合、",
       "または制限時間20秒を過ぎたら負けです。",
-      ""
+      shisa_start
     ]
-    messages.each_with_index do |message, i|
-      case i
-      when 0, 1, 3, 4, 5, 6, 7, 8, 9
-        puts message
-        sleep 1.5
-      when 2
-        favorite_food = ["ゴーヤー", "サーターアンダギー", "ソーキそば", "フーチャンプルー", "ちんすこう"].sample
-        puts "好きな食べ物は#{favorite_food}です。"
-        puts "-" * 40
-      else 10
-        puts "-" * 40
-        puts "それでは、わたくしシーサーから始めます！"
-        puts "=" * 40
-        sleep 3.0
-      end
+
+    messages.each do |message|
+      puts message
+      sleep 1.5
     end
+    sleep 1.5
   end
 end
