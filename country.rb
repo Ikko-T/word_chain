e57de752bf2e0c4b4f48157f17b78e8d61f1a2ef
Ikko-T <skyblueboy32@gmail.com>
2020-12-12 13:30:30 +0900

    Defined a class method in Country class.

--- country.rb
+++ country.rb
@@ -1,32 +1,34 @@
 class Country
-  COUNTRIES = File.read("./country.txt").split
+  class << self
+    COUNTRIES = File.read("./country.txt").split
 
-  def self.choose_random
-    COUNTRIES.sample
-  end
+    def choose_random
+      COUNTRIES.sample
+    end
 
-  def self.insert_history_record(country)
-    @history_record << country
-  end
+    def insert_history_record(country)
+      @history_record << country
+    end
 
-  def self.last_char
-    @history_record.last.delete('ー-').tr('ァィゥェォッャュョヮ', 'アイウエオツヤユヨワ')[-1]
-  end
+    def last_char
+      @history_record.last.delete('ー-').tr('ァィゥェォッャュョヮ', 'アイウエオツヤユヨワ')[-1]
+    end
 
-  def self.choose_intentional
-    countries = COUNTRIES.select { |c| c[0] == last_char }
-    countries.sample
-  end
+    def choose_intentional
+      countries = COUNTRIES.select { |c| c[0] == last_char }
+      countries.sample
+    end
 
-  def self.search_same(country)
-    COUNTRIES.find { |c| c == country }
-  end
+    def search_same(country)
+      COUNTRIES.find { |c| c == country }
+    end
 
-  def self.last_letter_fail?(country)
-    country[-1] == "ン"
-  end
+    def last_letter_fail?(country)
+      country[-1] == "ン"
+    end
 
-  def self.duplicate?
-    @history_record.count - @history_record.uniq.count > 0
+    def duplicate?
+      @history_record.count - @history_record.uniq.count > 0
+    end
   end
 end
